aa5aa2a15b37aa671d59adaf9fa23e73bc992bee
Dragonflydamsel01 <31666178+Dragonflydamsel01@users.noreply.github.com>
2023-08-23 17:52:20 -0400

    1 file modified

--- .eleventy.js
+++ .eleventy.js
@@ -152,9 +152,9 @@ module.exports = function (eleventyConfig) {
 
     // These are all optional, defaults are shown:
     dir: {
-      input: "content",          // default: "."
-			includes: "../_includes",  // default: "_includes"
-			data: "../_data",          // default: "_data"
+      input: "./content",          // default: "."
+			includes: "./_includes",  // default: "_includes"
+			data: "./_data",          // default: "_data"
 			output: "_site"
     },
   };
