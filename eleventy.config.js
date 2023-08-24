be7459f4d3411ec295325e6a67044a4c58d8f2f1
Dragonflydamsel01 <31666178+Dragonflydamsel01@users.noreply.github.com>
2023-08-24 00:47:27 -0400

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -153,9 +153,9 @@ module.exports = function (eleventyConfig) {
     // These are all optional, defaults are shown:
     dir: {
       input: "content",          // default: "."
-			includes: "_includes",  // default: "_includes"
-			data: "_data",          // default: "_data"
-			output: "_site"
+      includes: "_includes",  // default: "_includes"
+      data: "_data",          // default: "_data"
+      output: "_site"
     },
   };
 };
