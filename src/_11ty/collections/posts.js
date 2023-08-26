c0ab48c35ec3b6ebe92baa698be47d9ff3a3e31d
Dragonflydamsel01 <31666178+Dragonflydamsel01@users.noreply.github.com>
2023-08-26 12:33:00 -0400

    2 files modified, 18 added, 19 deleted

--- src/_11ty/collections/posts.js
+++ src/_11ty/collections/posts.js
@@ -1,5 +1,5 @@
 module.exports = function (collection) {
   return collection
-    .getFilteredByGlob(["./content/posts/*.html", "./content/posts/*.md"])
+    .getFilteredByGlob(["/content/posts/*.html", "/content/posts/*.md"])
     .reverse();
 };
